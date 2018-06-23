bf29606b045d8ef604ff695c6dd825d1c1df0f78
nystrome <nystrom.edwards@gmail.com>
2018-06-23 14:57:27 +0100

    Added tests for progress bar tool

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,8 @@
 """
 This module tests the functionality of the tools module.
 """
+import sys
+from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
 from gitissue import tools
@@ -86,3 +88,38 @@ class TestYesNoOption(TestCase):
     @patch('builtins.input', return_value='1')
     def test_single_number_option(self, mocked_input):
         self.assertFalse(tools.yes_no_option('Hear is a value'))
+
+
+class TestPrintProgressBar(TestCase):
+    def setUp(self):
+        self.held, sys.stdout = sys.stdout, StringIO()
+
+    def test_25_percent(self):
+        tools.print_progress_bar(1, 4)
+        expected_output = '\r |████████████--------------------------------------| 25.0% \r'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+
+    def test_33_percent(self):
+        tools.print_progress_bar(1, 3)
+        expected_output = '\r |████████████████----------------------------------| 33.3% \r'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+
+    def test_50_percent(self):
+        tools.print_progress_bar(1, 2)
+        expected_output = '\r |█████████████████████████-------------------------| 50.0% \r'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+
+    def test_66_percent(self):
+        tools.print_progress_bar(2, 3)
+        expected_output = '\r |█████████████████████████████████-----------------| 66.7% \r'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+
+    def test_75_percent(self):
+        tools.print_progress_bar(3, 4)
+        expected_output = '\r |█████████████████████████████████████-------------| 75.0% \r'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
+
+    def test_100_percent(self):
+        tools.print_progress_bar(2, 2)
+        expected_output = '\r |██████████████████████████████████████████████████| 100.0% \r \n'
+        self.assertEqual(sys.stdout.getvalue(), expected_output)
