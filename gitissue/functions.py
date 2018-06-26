4bcc25aa8bd0095c1e371a6dcbeee4ead7850ffd
nystrome <nystrom.edwards@gmail.com>
2018-06-26 14:03:12 +0100

    Create new issue number based on file contents and deserialise issue #1 #2 closes #3

--- gitissue/functions.py
+++ gitissue/functions.py
@@ -1,24 +1,30 @@
 import os
 import zlib
+import json
 
 from stat import S_IREAD
 from gitissue.errors import RepoObjectExistsError
 
 
-def get_location(object):
+def get_location(obj):
     # get the first two items in the sha for a folder
-    folder = object.repo.issue_objects_dir + '/' + str(object.hexsha)[:2]
+    folder = obj.repo.issue_objects_dir + '/' + str(obj.hexsha)[:2]
     # use the remainder of the string as a filename
-    filename = folder + '/' + str(object.hexsha)[2:]
+    filename = folder + '/' + str(obj.hexsha)[2:]
     return folder, filename
 
 
-def serialize(object):
-    folder, filename = get_location(object)
+def object_exists(obj):
+    folder, filename = get_location(obj)
+    return os.path.exists(filename)
+
+
+def serialize(obj):
+    folder, filename = get_location(obj)
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    data_to_write = str(object.data)
+    data_to_write = json.dumps(obj.data)
 
     # make the file, write compressed data, make read only
 
@@ -32,9 +38,23 @@ def serialize(object):
 
     # get the size of the file on the system
     stats = os.stat(filename)
-    object.size = stats.st_size
-    return object
+    obj.size = stats.st_size
+    return obj
 
 
-def deserialize(object):
-    raise NotImplementedError
+def deserialize(obj):
+    if obj.type == 'issue':
+        return deserialize_issue(obj)
+    else:
+        raise NotImplementedError
+
+
+def deserialize_issue(obj):
+    folder, filename = get_location(obj)
+    f = open(filename, 'rb')
+    contents = zlib.decompress(f.read()).decode()
+    f.close()
+    contents = json.loads(contents)
+    obj.data = contents
+
+    return obj
--- gitissue/issue.py
+++ gitissue/issue.py
@@ -1,25 +1,54 @@
 import hashlib
+import os
 
 from git import Object
 
-from gitissue.functions import serialize, deserialize
+from gitissue.functions import serialize, deserialize, object_exists
 from gitissue.errors import RepoObjectExistsError
 
 __all__ = ('Issue',)
 
 
+def get_new_issue_no(repo):
+    number_file = repo.issue_dir + '/NUMBER'
+
+    if not os.path.exists(number_file):
+        f = open(number_file, 'w')
+        f.write('0')
+        f.close()
+
+    f = open(number_file, 'r')
+    last_number = int(f.read())
+    f.close()
+
+    new_number = last_number + 1
+    f = open(number_file, 'w')
+    f.write(str(new_number))
+    f.close()
+
+    return new_number
+
+
 class Issue(Object):
+
+    __slots__ = ('data', 'filepath', 'contents', 'number', 'size')
 
     type = 'issue'
 
-    def __init__(self, data, repo, binsha):
+    def __init__(self, repo, binsha, data=None):
         super(Issue, self).__init__(repo, binsha)
-        if data is not None:
+        if not object_exists(self) and data is not None:
+            self.number = get_new_issue_no(repo)
+            data['number'] = self.number
             self.data = data
             self.filepath = data['filepath']
             self.contents = data['contents']
+            serialize(self)
         else:
             deserialize(self)
+            self.number = self.data['number']
+            self.filepath = self.data['filepath']
+            self.contents = self.data['contents']
 
     def __lt__(self, other):
         return str(self.hexsha) < str(other.hexsha)
@@ -28,9 +57,5 @@ class Issue(Object):
     def create(cls, repo, data):
         sha = hashlib.sha1(str(data).encode())
         binsha = sha.digest()
-        new_issue = cls(data, repo, binsha)
-        try:
-            serialize(new_issue)
-        except RepoObjectExistsError:
-            pass
+        new_issue = cls(repo, binsha, data)
         return new_issue
