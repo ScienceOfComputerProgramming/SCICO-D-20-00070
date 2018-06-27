8dd39984a38033cf7887266fb0f1490b5756c229
nystrome <nystrom.edwards@gmail.com>
2018-06-27 19:46:18 +0100

    Ad tests for issue trees #1 #2

--- gitissue/commit.py
+++ gitissue/commit.py
@@ -1,4 +1,5 @@
 import hashlib
+import re
 
 from git import util
 from git import Object
@@ -8,8 +9,41 @@ from git.util import hex_to_bin
 from gitissue import IssueTree
 from gitissue.functions import serialize, deserialize, object_exists
 from gitissue.errors import RepoObjectExistsError
+from gitissue.regex import MULTILINE_HASH_PYTHON_COMMENT
 
 __all__ = ("IssueCommit")
+
+
+def find_issues_in_commit_tree(commit_tree):
+    matches = []
+    if commit_tree.type != 'submodule':
+        for item in commit_tree:
+            if item.type == 'blob':
+
+                try:
+                    # read the data contained in that file
+                    object_contents = item.data_stream.read().decode('utf-8')
+
+                    # search for matches
+                    matched_issues = re.findall(
+                        MULTILINE_HASH_PYTHON_COMMENT,
+                        object_contents)
+
+                    # if a string match for issue found
+                    if matched_issues is not None:
+
+                        # create a dictionary with the results
+                        # and add full dict to list
+                        result = {'filepath': str(item.path),
+                                  'issues': matched_issues}
+                        matches.append(result)
+                except UnicodeDecodeError:
+                    pass
+            else:
+                # extend the list with the values to create
+                # one flat list of matches
+                matches.extend(find_issues_in_commit_tree(item))
+    return matches
 
 
 class IssueCommit(Object):
