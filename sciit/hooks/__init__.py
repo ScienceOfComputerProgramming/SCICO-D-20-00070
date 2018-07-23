9ece5a09c402220558a15bb977e67053ce7b2891
nystrome <nystrom.edwards@gmail.com>
2018-07-23 17:26:25 +0100

    Created an issue to define work to be done

---
+++ sciit/hooks/__init__.py
@@ -0,0 +1,10 @@
+"""
+@issue create git hooks for collaboration
+@description
+    These hooks should include those required
+    for building issue commits, issue trees, 
+    and issues from commits made by others.
+    It should create these between two commit
+    shas when conducting a pull, fetch or 
+    checkout.
+"""
