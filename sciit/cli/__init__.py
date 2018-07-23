39910e210dc1e57e6bacf8606d2bbe70e04f5ccb
nystrome <nystrom.edwards@gmail.com>
2018-07-23 14:07:13 +0100

    fixed the terminal colours for windows

---
+++ sciit/cli/__init__.py
@@ -0,0 +1,9 @@
+"""
+@issue fix errors associated with windows support
+@description 
+    There are errors that are caused when using sciit on windows. Fix errors 
+    pertaining to function call such as:
+        * shutil.rmtree
+        * colors on the terminal
+@label bug-fix
+"""
