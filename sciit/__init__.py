dc5c2668cc57b65a3d8dce3f95281c76426a752a
nystrome <nystrom.edwards@gmail.com>
2018-08-06 09:06:23 +0100

    added sciit ignore issue

--- sciit/__init__.py
+++ sciit/__init__.py
@@ -2,3 +2,14 @@ from .issue import Issue
 from .tree import IssueTree
 from .commit import IssueCommit
 from .repo import IssueRepo
+
+"""
+@issue sciit ignore
+@title Create a file the ignores issues in certain files
+@description
+    This feature may be a bit counter to the idea of the project
+    however it is always best to have users choose to exclude files
+    if they want too.
+@due date 07 August 2018
+@label feature
+"""
