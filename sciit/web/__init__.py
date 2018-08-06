52ba2bb4828457eea84d70c022e31e5d25bc31c7
nystrome <nystrom.edwards@gmail.com>
2018-08-06 11:13:18 +0100

    added issue for web interface

---
+++ sciit/web/__init__.py
@@ -0,0 +1,9 @@
+"""
+@issue local web interface
+@title Create a Local Web Interface for sciit
+@description
+    The interface should include only the features that allow you to view the issue tracker
+    at this time. Load the build history object into the web static memory to be used by all
+    routes that access the website.
+@label feature
+"""
