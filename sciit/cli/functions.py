f563caa12abd96c576e8e4233e69349f468d0fed
twsswt <timothywstorer@googlemail.com>
2018-10-16 12:45:02 +0100

    Tidies up Hooks Code.

--- sciit/cli/functions.py
+++ sciit/cli/functions.py
@@ -3,6 +3,11 @@
 import pydoc
 import pkg_resources
 
+from sciit.functions import get_sciit_ignore_path_spec
+from sciit.commit import find_issue_snapshots_in_commit_paths_that_changed
+from sciit.errors import RepoObjectDoesNotExistError
+from .color import CPrint
+
 
 def page(output):
     pydoc.pipepager(output, cmd='less -FRSX')
@@ -20,6 +25,13 @@ def read_sciit_version():
     filename = pkg_resources.resource_filename('sciit.man', 'VERSION')
     with open(filename, 'rb') as f:
         return f.read().decode('utf-8')
+
+
+def do_repository_is_init_check(issue_repository):
+    if not issue_repository.is_init():
+        CPrint.bold_red('Error: Issue repository not setup.')
+        print('Solve this error by building issue repository using: git sciit init')
+        exit(127)
 
 
 def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='#'):
@@ -42,3 +54,56 @@ def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, lengt
 
     if iteration == total:
         print()
+
+
+def do_commit_contains_duplicate_issue_filepaths_check(issue_repository, commit):
+
+    git_repository = issue_repository.git_repository
+
+    ignored_files = get_sciit_ignore_path_spec(issue_repository.git_repository)
+
+    issue_snapshots, changed_paths = \
+        find_issue_snapshots_in_commit_paths_that_changed(commit, ignore_files=ignored_files)
+
+    if len(set(issue_snapshots)) != len(issue_snapshots):
+        file_paths_by_issue_id = dict()
+
+        for issue_snapshot in issue_snapshots:
+            issue_id = issue_snapshot.issue_id
+            if issue_id not in file_paths_by_issue_id:
+                file_paths_by_issue_id[issue_id] = list()
+            file_paths_by_issue_id[issue_id].append(issue_snapshot.filepath)
+
+        duplicates =\
+            {issue_id: file_paths for issue_id, file_paths in file_paths_by_issue_id.items() if len(file_paths) > 1}
+
+        for (issue_id, file_paths) in duplicates.items():
+            CPrint.bold_red(f'Duplicate Issue: {issue_id}')
+            for file_found in file_paths:
+                CPrint.red(f'\tfound in {file_found}')
+
+        git_repository.git.execute(['git', 'reset', 'HEAD~1', '--soft'])
+        CPrint.bold_red(f'HEAD @: {git_repository.head.commit.summary} ~ {git_repository.head.commit.hexsha[:7]}')
+        exit()
+
+
+def print_status_summary(issue_repository):
+    try:
+        location = issue_repository.git_repository.head.ref.name
+    except TypeError:
+        location = 'DETACHED:' + issue_repository.git_repository.head.commit.hexsha
+
+    try:
+        issues = issue_repository.get_open_issues()
+        # show issue status at head to the user
+        if len(issues) > 0:
+            result = str(len(issues)) + ' Open Issues @' + location
+            CPrint.bold_red(result)
+        else:
+            result = 'No Open Issues @' + location
+            CPrint.bold_green(result)
+
+    except RepoObjectDoesNotExistError as error:
+        CPrint.bold_red(error)
+        print('Solve error by rebuilding issue repository using: git sciit init -r')
+        exit(127)
