aeec6dc94281381109ab39fde5e3b389a104e950
twsswt <timothywstorer@googlemail.com>
2018-10-31 13:15:18 +0000

    Updates Feature Demonstration Script

--- demonstration.sh
+++ demonstration.sh
@@ -7,6 +7,7 @@ git-sciit.exe init
 touch README.md
 git add README.md
 git commit -m "Initial import."
+
 git checkout -b photo-upload-on-claim
 mkdir -p docs/issues
 cat > docs/issues/photo-upload-on-claim.md <<-EOF
@@ -25,14 +26,46 @@ cat > docs/issues/photo-upload-on-claim.md <<-EOF
 EOF
 git add docs/issues
 git commit -m "Adds Photo Upload on Claim"
-git-sciit.exe tracker
+git-sciit.exe status -f
+git-sciit.exe issue photo-upload-on-claim
+
 git checkout master
 git merge photo-upload-on-claim
-git-sciit.exe tracker
+git-sciit.exe status -f
+git-sciit.exe issue photo-upload-on-claim
+
 git checkout photo-upload-on-claim 
+mkdir -p ensure
+cat > ensure/main.py <<-EOF
+"""
+@issue photo-upload-on-claim
+@title Photo Upload on Claim
+@description
+ We need a photo upload 
+ feature so that customers can
+ provide supplemental evidence
+ for their insurance claim, 
+ e.g. of damage during a Road
+ Traffic Accident.
+@priority medium
+"""
+EOF
+
+git add ensure/main.py
 git rm docs/issues/photo-upload-on-claim.md
+git commit -m "Begins Work on Photo Upload on Claim"
+git-sciit.exe status -f
+git-sciit.exe issue photo-upload-on-claim
+
+rm ensure/main.py
+touch ensure/main.py
+git add ensure/main.py
 git commit -m "Closes Photo Upload on Claim"
-git-sciit.exe tracker
+git-sciit.exe status -f
+git-sciit.exe issue photo-upload-on-claim
+
 git checkout master
 git merge photo-upload-on-claim
-git-sciit.exe tracker -a
+git-sciit.exe status -f
+git-sciit.exe issue photo-upload-on-claim
+
