f18d7e38fe8f094696b54e220b7519a9a44b8785
timothywstorer@googlemail.com <timothywstorer@googlemail.com>
2020-04-21 11:50:43 +0100

    Extends Demonstration Project Script

--- demonstration.sh
+++ demonstration.sh
@@ -1,12 +1,17 @@
+# Setup
+
 rm -rf demo
 mkdir demo
 cd demo
 rm -rf .git/
 git init
-git-sciit.exe init
+python ../runner.py init
 touch README.md
 git add README.md
 git commit -m "Initial import."
+
+
+# Add first issue
 
 git checkout -b photo-upload-on-claim
 mkdir -p backlog
@@ -26,17 +31,25 @@ cat > backlog/photo-upload-on-claim.md <<-EOF
 EOF
 
 git add backlog
-git commit -m "Adds Photo Upload on Claim"
-git-sciit.exe status -f
-git-sciit.exe issue photo-upload-on-claim
+git commit -m "Adds new issue Photo Upload on Claim"
+python ../runner.py status -f
+python ../runner.py issue photo-upload-on-claim
+
+# Accept first issue
 
 git checkout master
 git merge photo-upload-on-claim
-git-sciit.exe status -f
-git-sciit.exe issue photo-upload-on-claim
+python ../runner.py status -f
+python ../runner.py issue photo-upload-on-claim
+
+# Add three sub-issues
+
+
+## First sub-issue
 
 git checkout photo-upload-on-claim
 git checkout -b photo-upload-on-claim-uat
+
 mkdir features
 cat > features/claim.feature <<-EOF
 #***
@@ -49,6 +62,61 @@ cat > features/claim.feature <<-EOF
 #***
 EOF
 
+git add features/claim.feature
+git commit -m "Creates new issue to create a scenario for Small JPG Upload on Claims"
+python ../runner.py issue photo-upload-on-claim-uat
+
+## Second sub-issue
+
+git checkout photo-upload-on-claim
+git checkout -b photo-upload-on-claim-db
+
+mkdir insure_and_go
+cat > insure_and_go/db.py <<-EOF
+"""
+@issue photo-upload-on-claim-db
+@title Extend Database Schema for Photo Uploads
+@description
+ Claim schema needs to be updated for photo
+ upload meta-data, including
+ 
+ - Filepath
+ - Description
+
+"""
+
+EOF
+
+git add insure_and_go/db.py
+git commit -m "Creates new issue to update database schema"
+python ../runner.py issue photo-upload-on-claim-db
+
+
+## Third sub-issue
+
+git checkout photo-upload-on-claim
+git checkout -b photo-upload-on-claim-view
+
+mkdir insure_and_go
+cat > insure_and_go/views.py <<-EOF
+"""
+@issue photo-upload-on-claim-view
+@title Extend Claim View to Handle Photos
+@description
+ Extend claim view and form to handle
+ photo attachments.
+"""
+
+EOF
+
+git add insure_and_go/views.py
+git commit -m "Creates new issue to update claim view"
+python ../runner.py issue photo-upload-on-claim-view
+
+
+## Add dependencies to main issue
+
+git checkout photo-upload-on-claim
 cat > backlog/photo-upload-on-claim.md <<-EOF
 ---
 @issue photo-upload-on-claim
@@ -60,50 +128,30 @@ cat > backlog/photo-upload-on-claim.md <<-EOF
  for their insurance claim, 
  e.g. of damage during a Road
  Traffic Accident.
-@blockers photo-upload-on-claim-uat, photo-upload-on-claim-db, photo-upload-on-claim-views
+@blockers photo-upload-on-claim-uat, photo-upload-on-claim-db, photo-upload-on-claim-view
 @priority medium
 ---
 EOF
 
-git add features/claim.feature
 git add backlog/photo-upload-on-claim.md
 git commit -m "Begins Work on Photo Upload on Claim"
-git-sciit.exe status -f
+python ../runner.py status -f
+
+## Accept sub-issues
 
 git checkout photo-upload-on-claim
 git merge photo-upload-on-claim-uat
+git merge photo-upload-on-claim-db
+git merge photo-upload-on-claim-view
+
 git checkout master
 git merge photo-upload-on-claim
-git-sciit.exe tracker
+python ../runner.py tracker
+
+
+# Close first sub-issue
 
 git checkout photo-upload-on-claim-uat
-
-cat > features/claim.feature <<-EOF
-#***
-# @issue photo-upload-on-claim-uat
-# @title Photo Upload on Claim UAT
-# @description
-#  Extend existing claim user stories
-#   with scenarios that include photo
-#   upload.
-#***
-
-Feature: Photo upload for claims
-Scenario: Small JPEG Upload with Description
- Given a claim
- And a small JPEG 
- And a description
- When I select the photograph
- And I enter a description
- And I click submit
- Then the photograph is stored
- And a database entry is created.
-
-EOF
-
-git add features/claim.feature
-git commit -m "Creates new Scenario for Small JPG Upload on Claims"
-git sciit issue photo-upload-on-claim-uat
 
 cat > features/claim.feature <<-EOF
 
@@ -122,26 +170,59 @@ EOF
 
 git add features/claim.feature
 git commit -m "Closes photo-upload-on-claim-uat"
-git sciit issue photo-upload-on-claim-uat
+python ../runner.py issue photo-upload-on-claim-uat
+
+# Close second sub-issue
+
+git checkout photo-upload-on-claim-db
+rm insure_and_go/db.py
+touch insure_and_go/db.py
+git add insure_and_go/db.py
+git commit -m "Closes photo-upload-on-claim-db"
+
+# Close third sub-issue
+
+git checkout photo-upload-on-claim-view
+rm insure_and_go/views.py
+touch insure_and_go/views.py
+git add insure_and_go/views.py
+git commit -m "Closes photo-upload-on-claim-view"
+
+python ../runner.py tracker
+
+# Merge closed issues to master
 
 git checkout photo-upload-on-claim
 git merge photo-upload-on-claim-uat
-git sciit issue photo-upload-on-claim-uat
+git merge photo-upload-on-claim-db
+git merge photo-upload-on-claim-view
+
+python ../runner.py tracker
 
 git checkout master
 git merge photo-upload-on-claim
-git sciit tracker 
+python ../runner.py tracker
+
+# Close main issue
 
 git checkout photo-upload-on-claim
 git rm backlog/photo-upload-on-claim.md
 git commit -m "Closes photo-upload-on-claim"
+
 git checkout photo-upload-on-claim-uat
 git merge photo-upload-on-claim
+
+git checkout photo-upload-on-claim-db
+git merge photo-upload-on-claim
+
+git checkout photo-upload-on-claim-view
+git merge photo-upload-on-claim
+
 git checkout photo-upload-on-claim
-git sciit issue photo-upload-on-claim
+python ../runner.py issue photo-upload-on-claim
 
 git checkout master
 git merge photo-upload-on-claim
-git sciit issue photo-upload-on-claim
+python ../runner.py issue photo-upload-on-claim
 
-git sciit tracker -fa
+python ../runner.py tracker -fa
